1678c5e7dba396f6ddbe02e82f0c673dfca2037c
Dana Alami <dana.alami18@gmail.com>
2021-03-04 20:35:35 +0200

    set up code to get password length requested

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
-    return "password"
+function generatePassword() {
+  var minLength = 8;
+  var maxLength = 128;
+  var lowerCaseChar = "abcdefghijklmnopqrstuvxyz";
+  var upperCaseChar = "ABCDEFGHIJKLMNOQPRSTUVWXYZ";
+  var numbers = "1234567890";
+  var special = "!@£$%^&*()";
+
+  var lengthRequested = parseInt(prompt("How long would you like your password to be?"));
+
+  if (minLength > lengthRequested) {
+    alert("You have entered a number less than 8... please try again!");
+    return "TRY AGAIN"
+  }
+  if (maxLength < lengthRequested) {
+    alert("You have entred a number greater than 128... please try again");
+    return "Try Again"
+  }
+
+  return "password"
 }
 
 // Write password to the #password input
